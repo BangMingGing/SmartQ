40aa784559b16d56167837ad67856b4e80b1aa54
minguencho <80305473+minguencho@users.noreply.github.com>
2022-09-28 09:41:31 +0900

    test file for DB

--- test4DB.py
+++ test4DB.py
@@ -4,22 +4,6 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/Spec', methods=['GET'])
-def Spec():
-    with open('Specification.py', 'rb') as f:
-        exp_spec = f.read()
-
-    with open('model_spec.py', 'rb') as f:
-        model_spec = f.read()
-
-    msg = {"header" : "Spec",
-            'Spec_name': 'test',
-            "exp_spec" : 'exp_spec',
-            "model_spec" : 'model_spec'}
-
-    return pickle.dumps(json.dumps(msg))
-
-
 @app.route('/test', methods=['GET'])
 def test():
     msg = {'Encoded_Tensor_size' : '0.8',
@@ -27,16 +11,6 @@ def test():
             'time_unit' : 'ms',
             'Tensor_unit' : 'mb'
                 }
-    msg_map = {'device_num' : 1,
-                'Spec_name' : 'test'}
-location_info ={'device num' = 1,
-'altitude' = 1,
-'lat' = 1,
-'lon' = 1,
-'speed' =1,
-'battery' =1
-}
-
     msg = {"header" : "test_results",
             "mapping_info" : json.dumps(msg_map),
             "json_file" : json.dumps(msg)}
