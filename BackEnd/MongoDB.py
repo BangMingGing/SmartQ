27dde7270699fb454e5f03a9f85563a09c14cb50
minguencho <80305473+minguencho@users.noreply.github.com>
2022-11-27 16:22:19 +0900

    Update MongoDB.py

--- BackEnd/MongoDB.py
+++ BackEnd/MongoDB.py
@@ -8,15 +8,12 @@ RABBITMQ_SERVER_PORT = '5672'
 MONGODB_SERVER_IP = '203.255.57.129'
 MONGODB_SERVER_PORT = 27017
 
-
 class MongoDB():
     def __init__(self, queue_name='MongoDB', exchange_name='output', routing_key='toMongoDB'):
         self.credentials = pika.PlainCredentials('rabbitmq', '1q2w3e4r')
         self.connection = pika.BlockingConnection(pika.ConnectionParameters(RABBITMQ_SERVER_IP, RABBITMQ_SERVER_PORT, 'vhost', self.credentials))
         self.channel = self.connection.channel()
-
         self.queue_name = queue_name
-
         queue = self.channel.queue_declare(queue_name)
         self.channel.queue_bind(exchange=exchange_name, queue=queue_name, routing_key=routing_key)
 
@@ -34,26 +31,21 @@ class MongoDB():
         else:
             col.insert_one(data)
 
-    
     def callback(self, ch, method, properties, body):
         message = pickle.loads(body, encoding='bytes')['message']
-
+        
         # DB에 저장하는 코드
         self.save_data(message)
         print(f'[MongoDB] message : {message}')
 
         ch.basic_ack(delivery_tag=method.delivery_tag)
 
-    
     def consume(self):
         self.channel.basic_consume(on_message_callback=self.callback, queue=self.queue_name)
         print('[MongoDB] Start Consuming')
         self.channel.start_consuming()
 
-
-
 if __name__ == '__main__':
-    
     process = MongoDB()
     process.consume()
 
